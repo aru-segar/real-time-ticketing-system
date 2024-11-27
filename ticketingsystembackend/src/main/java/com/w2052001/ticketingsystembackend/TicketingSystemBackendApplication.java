b96e8be51d2c4abb5645e9df36acb3b4b5c6ae05
Aruniga Gnanasegaran <166505613+aru-segar@users.noreply.github.com>
2024-11-27 12:37:02 +0530

    Add files via upload

---
+++ ticketingsystembackend/src/main/java/com/w2052001/ticketingsystembackend/TicketingSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.w2052001.ticketingsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketingSystemBackendApplication.class, args);
+	}
+
+}
---
+++ ticketingsystembackend/src/test/java/com/w2052001/ticketingsystembackend/TicketingsystembackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w2052001.ticketingsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingsystembackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
